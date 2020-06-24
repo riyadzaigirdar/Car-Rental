75e79f8191035d257ec23ec724e8a190878bcc2f
Rahidul Islam <rahidul_islam@criptext.com>
2020-06-24 10:53:05 +0600

    password placeholder not found

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import User
-from django.forms import EmailInput, PasswordInput
+from django.forms import EmailInput, PasswordInput, TextInput
 
 
 class SignUpForm(UserCreationForm):
-    
     class Meta:
         model = User
-        fields = ["email","password1", "password2"]
+        fields = ["email", "username", "phone", "password1", "password2"]
         widgets = {
-            'email': EmailInput(attrs={'class': 'frm-group', 'placeholder':'Your Email'}),
-            'password1': PasswordInput(attrs={'class': 'frm-group', 'placeholder':'Your Password'}),
-            'password2': PasswordInput(attrs={'class': 'frm-group', 'placeholder':'Enter Re Password'}),
+            'email': EmailInput(attrs={'placeholder': 'Your Email'}),
+            'username': TextInput(attrs={'placeholder': 'Your Username'}),
+            'phone': TextInput(attrs={'placeholder': 'Your Phone'}),
+            'password1': PasswordInput(attrs={'placeholder': 'Your Password'}),
+            'password2': PasswordInput(attrs={'placeholder': 'Enter Re Password'}),
         }
+
