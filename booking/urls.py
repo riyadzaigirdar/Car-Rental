a536ca60e4a23ae09decda90385e100a7a7976b2
Rahidul Islam <rahidul_islam@criptext.com>
2020-07-09 12:01:16 +0600

    all is ok

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from booking import views
-
+from django.contrib.auth.decorators import login_required
 app_name = 'booking'
 
 urlpatterns = [
     #path('add/', views.BookingView.as_view(), name='add_reservation'),
-    path('add/<int:pk>/', views.BookingView.as_view(), name='add_reservation'),
+    path('add/<int:pk>/', login_required(views.BookingView.as_view()), name='add_reservation'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -11,6 +11,7 @@ class BookingView(LoginRequiredMixin, CreateView):
     form_class = BookingCarForm
     template_name = 'booking/reservation.html'
     success_url = 'home:home'
+    #redirect_field_name = 'next'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
